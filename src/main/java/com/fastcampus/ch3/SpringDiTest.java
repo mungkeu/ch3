a76a1302dc7b429f9cc4a88d5b878f513779cb05
mungkeu <101312446+mungkeu@users.noreply.github.com>
2022-08-30 14:58:51 +0900

    Spring : Database Connection
    CRUD

--- src/main/java/com/fastcampus/ch3/SpringDiTest.java
+++ src/main/java/com/fastcampus/ch3/SpringDiTest.java
@@ -1,91 +1,91 @@
-package com.fastcampus.ch3;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-
-@Component("engine") class Engine{} // <bean id="engine" class="com.fastcampus.ch3.Engine"/>과 같다.
-@Component class SuperEngine extends Engine{}
-@Component class TurboEngine extends Engine{}
-@Component class Door{}
-@Component
-class Car{
-    @Value("red")
-    String color;
-    @Value("100")
-    int oil;
-    @Autowired
-    @Qualifier("superEngine") // 여러개면 이렇게 지정할수도 있다.
-    Engine engine; // byType - @Autowired 는 타입으로 먼저 검색, 여러개면 이름으로 검색한다. - engine, superEngine, turboEngine
-    // 위의 2개를 쓴것과 비슷한 방법 Resorce(name="superEngine") // byName
-    @Autowired Door[] doors;  // byType
-
-    // 기본 생성자는 꼭 만들어주자.
-    public Car(){}
-    public Car(String color, int oil, Engine engine, Door[] doors) {
-        this.color = color;
-        this.oil = oil;
-        this.engine = engine;
-        this.doors = doors;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setOil(int oil) {
-        this.oil = oil;
-    }
-
-    public void setEngine(Engine engine) {
-        this.engine = engine;
-    }
-
-    public void setDoors(Door[] doors) {
-        this.doors = doors;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "color='" + color + '\'' +
-                ", oil=" + oil +
-                ", engine=" + engine +
-                ", doors=" + Arrays.toString(doors) +
-                '}';
-    }
-}
-
-public class SpringDiTest {
-
-    public static void main(String[] args) {
-        ApplicationContext ac = new GenericXmlApplicationContext("config1.xml");
-        Car car = (Car) ac.getBean("car"); // byName 아래와 같다 즉 이름으로하든 타입으로하든 같다.
-//        Car car2 = (Car) ac.getBean(Car.class); // byType
-//        Car car3 = ac.getBean("car", Car.class); // 이름과 타입을 둘다 보내므로 형변환하지 않아도 된다.
-        //Engine engine = (Engine) ac.getBean("engine"); // byName
-        //Engine engine = (Engine) ac.getBean(Engine.class); // byType
-//        Engine engine = (Engine) ac.getBean("superEngine"); // 같은 타입이 여러개라면 이름으로 찾는다.
-//        Door door = (Door) ac.getBean("door");
-
-        // 아래의 car는 bean의 기본값이 싱글톤으로 다 같은 해시값을 가진다.
-        // 만일 다르게 하고싶다면 xml에서 스코프를 프로토타입으로 바꾸어준다.
-        System.out.println("car = " + car);
-//        System.out.println("car2 = " + car2);
-//        System.out.println("car3 = " + car3);
-//        System.out.println("engine = " + engine);
-//        System.out.println("door = " + door);
-
-//        car.setColor("red");
-//        car.setOil(100);
-//        car.setEngine(engine);
-//        car.setDoors(new Door[]{ac.getBean("door", Door.class), ac.getBean("door", Door.class)});
-//        위의 세터를 이용하지 않아도 프로퍼티에서 설정할수도 있다.
-        System.out.println("car = " + car);
-    }
-}
+//package com.fastcampus.ch3;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.GenericXmlApplicationContext;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Arrays;
+//
+//@Component("engine") class Engine{} // <bean id="engine" class="com.fastcampus.ch3.Engine"/>과 같다.
+//@Component class SuperEngine extends Engine{}
+//@Component class TurboEngine extends Engine{}
+//@Component class Door{}
+//@Component
+//class Car{
+//    @Value("red")
+//    String color;
+//    @Value("100")
+//    int oil;
+//    @Autowired
+//    @Qualifier("superEngine") // 여러개면 이렇게 지정할수도 있다.
+//    Engine engine; // byType - @Autowired 는 타입으로 먼저 검색, 여러개면 이름으로 검색한다. - engine, superEngine, turboEngine
+//    // 위의 2개를 쓴것과 비슷한 방법 Resorce(name="superEngine") // byName
+//    @Autowired Door[] doors;  // byType
+//
+//    // 기본 생성자는 꼭 만들어주자.
+//    public Car(){}
+//    public Car(String color, int oil, Engine engine, Door[] doors) {
+//        this.color = color;
+//        this.oil = oil;
+//        this.engine = engine;
+//        this.doors = doors;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//
+//    public void setOil(int oil) {
+//        this.oil = oil;
+//    }
+//
+//    public void setEngine(Engine engine) {
+//        this.engine = engine;
+//    }
+//
+//    public void setDoors(Door[] doors) {
+//        this.doors = doors;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Car{" +
+//                "color='" + color + '\'' +
+//                ", oil=" + oil +
+//                ", engine=" + engine +
+//                ", doors=" + Arrays.toString(doors) +
+//                '}';
+//    }
+//}
+//
+//public class SpringDiTest {
+//
+//    public static void main(String[] args) {
+//        ApplicationContext ac = new GenericXmlApplicationContext("config1.xml");
+//        Car car = (Car) ac.getBean("car"); // byName 아래와 같다 즉 이름으로하든 타입으로하든 같다.
+////        Car car2 = (Car) ac.getBean(Car.class); // byType
+////        Car car3 = ac.getBean("car", Car.class); // 이름과 타입을 둘다 보내므로 형변환하지 않아도 된다.
+//        //Engine engine = (Engine) ac.getBean("engine"); // byName
+//        //Engine engine = (Engine) ac.getBean(Engine.class); // byType
+////        Engine engine = (Engine) ac.getBean("superEngine"); // 같은 타입이 여러개라면 이름으로 찾는다.
+////        Door door = (Door) ac.getBean("door");
+//
+//        // 아래의 car는 bean의 기본값이 싱글톤으로 다 같은 해시값을 가진다.
+//        // 만일 다르게 하고싶다면 xml에서 스코프를 프로토타입으로 바꾸어준다.
+//        System.out.println("car = " + car);
+////        System.out.println("car2 = " + car2);
+////        System.out.println("car3 = " + car3);
+////        System.out.println("engine = " + engine);
+////        System.out.println("door = " + door);
+//
+////        car.setColor("red");
+////        car.setOil(100);
+////        car.setEngine(engine);
+////        car.setDoors(new Door[]{ac.getBean("door", Door.class), ac.getBean("door", Door.class)});
+////        위의 세터를 이용하지 않아도 프로퍼티에서 설정할수도 있다.
+//        System.out.println("car = " + car);
+//    }
+//}
---
+++ src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -0,0 +1,181 @@
+package com.fastcampus.ch3;
+
+import org.apache.tomcat.dbcp.dbcp2.DelegatingPreparedStatement;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+import org.springframework.jdbc.datasource.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DBConnectionTest2Test {
+    @Autowired
+    DataSource ds;
+
+    @Test
+    public void insertUserTest() throws Exception{
+        User user = new User("asdf3","1234","abc","aaaa@aaa.com", new Date(), "fb", new Date());
+        deleteAll();
+        int rowCnt = insertUser(user);
+
+        assertTrue(rowCnt==1);
+    }
+
+    @Test
+    public void selectUserTest() throws Exception{
+//        List<User>list = selectAll();
+
+//        for(User user : list){
+//            System.out.print(user.getId());
+//            System.out.print(user.getPwd());
+//            System.out.print(user.getName());
+//            System.out.print(user.getEmail());
+//            System.out.print(user.getBirth());
+//            System.out.print(user.getSns());
+//            System.out.print(user.getReg_date());
+//            System.out.println();
+//        }
+//        assertTrue(list!=null);
+        deleteAll();
+        User user = new User("asdf3","1234","abc","aaaa@aaa.com", new Date(), "fb", new Date());
+        insertUser(user);
+        user = selectUser("asdf3");
+        assertTrue(user.getId().equals("asdf3"));
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception{
+        deleteAll();
+        int rowCnt = deleteUser("asdf");
+        assertTrue(rowCnt==0);
+        User user = new User("asdf3","1234","abc","aaaa@aaa.com", new Date(), "fb", new Date());
+        rowCnt = insertUser(user);
+        assertTrue(rowCnt==1);
+        rowCnt = deleteUser(user.getId());
+        assertTrue(rowCnt==1);
+        assertTrue(selectUser(user.getId())==null);
+    }
+
+    @Test
+    public void updateUserTest() throws Exception{
+        deleteAll();
+        User user = new User("asdf3","1234","abc","aaaa@aaa.com", new Date(), "fb", new Date());
+        insertUser(user);
+
+        int rowCnt = updateUser(user);
+        assertTrue(rowCnt==1);
+    }
+    // 매개변수로 받은 사용자 정보로 user_info 테이블을 update하는 메서드
+    public int updateUser(User user) throws Exception{
+        Connection conn = ds.getConnection();
+        String sql = "update user_info set pwd=?,name=? where id=?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, "test");
+        pstmt.setString(2, "test");
+        pstmt.setString(3, user.getId());
+        int rowCnt = pstmt.executeUpdate();
+        return rowCnt;
+    }
+    public int deleteUser(String id) throws Exception{
+        Connection conn = ds.getConnection();
+        String sql = "delete from user_info where id=?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1,id);
+
+        return pstmt.executeUpdate();
+    }
+    private User selectUser(String id) throws Exception{
+        Connection conn = ds.getConnection();
+        String sql = "select * from user_info where id = ?";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1,id);
+        ResultSet rs = pstmt.executeQuery();
+
+        if(rs.next()){
+            User user = new User();
+            user.setId(rs.getString(1));
+            user.setPwd(rs.getString(2));
+            user.setName(rs.getString(3));
+            user.setEmail(rs.getString(4));
+            user.setBirth(rs.getDate(5));
+            user.setSns(rs.getString(6));
+            user.setReg_date(rs.getDate(7));
+            return user;
+        }
+        return null;
+    }
+
+    private List<User> selectAll() throws Exception{
+        List<User> list = new ArrayList<>();
+        Connection conn = ds.getConnection();
+        String sql = "select * from user_info";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+
+        while(rs.next()){
+            User user = new User();
+            user.setId(rs.getString(1));
+            user.setPwd(rs.getString(2));
+            user.setName(rs.getString(3));
+            user.setEmail(rs.getString(4));
+            user.setBirth(rs.getDate(5));
+            user.setSns(rs.getString(6));
+            user.setReg_date(rs.getDate(7));
+            list.add(user);
+        }
+        return list;
+    }
+    private void deleteAll() throws Exception{
+        Connection conn = ds.getConnection();
+
+        String sql = "delete from user_info";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.executeUpdate();
+    }
+
+    // 사용자 정보를 user_info 테이블에 저장하는 메서드
+    public int insertUser(User user) throws Exception{
+        Connection conn = ds.getConnection();
+
+//        insert into user_info (id, pwd, name, email, birth, sns, reg_date)
+//        values ('asdf1','1234','smith','aaa@aaa.com','2021-01-01','fascbook',now());
+
+        String sql = "insert into user_info values (?,?,?,?,?,?,now())";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1,user.getId());
+        pstmt.setString(2,user.getPwd());
+        pstmt.setString(3,user.getName());
+        pstmt.setString(4,user.getEmail());
+        pstmt.setDate(5,new java.sql.Date(user.getBirth().getTime()));
+        pstmt.setString(6,user.getSns());
+
+        int rowCnt = pstmt.executeUpdate();
+        return rowCnt;
+    }
+    @Test
+    public void jdbcConnectionTest() throws Exception {
+//        ApplicationContext ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/**/root-context.xml");
+//        DataSource ds = ac.getBean(DataSource.class);
+
+        Connection conn = ds.getConnection(); // 데이터베이스의 연결을 얻는다.
+
+        System.out.println("conn = " + conn);
+        assertTrue(conn!=null); // 괄호 안의 조건식이 true먄, 테스트 성공, 아니면 실패
+    }
+}
